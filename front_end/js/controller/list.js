7c20b2cd5823cfdcf8891fdf56faf13960c781ac
dgdheeraj <gharde.dheeraj@gmail.com>
2020-04-11 15:40:18 +0530

    Basic front_End template added

---
+++ front_end/js/controller/list.js
@@ -0,0 +1,12 @@
+(function(){
+
+    angular
+      .module("VocBuild")
+      .controller("listCtrl", ListController);
+  
+    function ListController(){
+      // List Controller Logic
+      this.gw="fe";
+    }
+  
+  })();
---
+++ front_end/js/controller/quiz.js
@@ -0,0 +1,75 @@
+(function(){
+    angular.module("VocBuild").controller("quizCtrl",function(){
+        /*var quizQuestions  = [
+            {
+              type: "text",
+              text: "How much can a loggerhead weigh?",
+              possibilities: [
+                  {
+                      answer: "Up to 20kg"
+                  },
+                  {
+                      answer: "Up to 115kg"
+                  },
+                  {
+                      answer: "Up to 220kg"
+                  },
+                  {
+                      answer: "Up to 500kg"
+                  }
+              ],
+              selected: null,
+              correct: null
+            },
+            {
+              type: "text",
+              text: "What is the typical lifespan of a Green Sea Turtle?",
+              possibilities: [
+                  {
+                      answer: "150 years"
+                  },
+                  {
+                      answer: "10 years"
+                  },
+                  {
+                      answer: "80 years"
+                  },
+                  {
+                      answer: "40 years"
+                  }
+              ],
+              selected: null,
+              correct: null
+            },
+            {
+              type: "text",
+              text: "Which of these turtles are herbivores?",
+              possibilities: [
+                  {
+                      answer: "Loggerhead Turtle"
+                  },
+                  {
+                      answer: "Hawksbill Turtle"
+                  },
+                  {
+                      answer: "Leatherback Turtle"
+                  },
+                  {
+                      answer: "Green Turtle"
+                  }
+              ],
+              selected: null,
+              correct: null
+            }
+        ];*/
+        self=this;
+        self.notes=[
+            {id:1,label:"First Note",done:false},
+            {id:2,label:"Second Note",done:true},
+            {id:3,label:"Third Note",done:true},
+            {id:3,label:"Fourth Note",done:false},
+        ];
+        
+        
+    })
+})();
